19706b36da08446bb26c9badd1b4b4db93d70f12
skitts24 <39681481+skitts24@users.noreply.github.com>
2020-03-08 01:13:44 +0800

    Update corsair-telegraf.sh

--- corsair-telegraf.sh
+++ corsair-telegraf.sh
@@ -12,6 +12,11 @@ function rail() {
         echo "$RAW" | grep "$1" | grep "$2" | tr -s ' ' |  cut -d ' ' -f 3
 }
 
+# Details
+name=$(simple "name")
+vendor=$(simple "vendor")
+
+#PSU Status
 temp_0=$(simple "temp1")
 temp_1=$(simple "temp2")
 powered=$(simple "powered")
@@ -34,12 +39,8 @@ volts_3_3v=$(rail "output2 volts")
 amps_3_3v=$(rail "output2 amps")
 watts_3_3v=$(rail "output2 watts")
 
-# InfluxDB output
-#With quotes
-#echo "power_supply temp_0=\"$temp_0\",temp_1=\"$temp_1\",total_time=\"$powered\",uptime=\"$uptime\",supply_volts=\"$supply\",total_watts=\"$total_watts\",12v_volts=\"$volts_12v\",12v_amps=\"$amps_12v\",12v_watts=\"$watts_12v\",5v_volts=\"$volts_5v\",5v_amps=\"$amps_5v\",5v_watts=\"$watts_5v\",3.3v_volts=\"$volts_3_3v\",3.3v_amps=\"$amps_3_3v\",3.3v_watts=\"$watts_3_3v\" $(date '+%s%3N')" > $OUTPUT
-
-#No quotes
-echo "power_supply temp_0=$temp_0,temp_1=$temp_1,total_time=$powered,uptime=$uptime,supply_volts=$supply,total_watts=$total_watts,12v_volts=$volts_12v,12v_amps=$amps_12v,12v_watts=$watts_12v,5v_volts=$volts_5v,5v_amps=$amps_5v,5v_watts=$watts_5v,3.3v_volts=$volts_3_3v,3.3v_amps=$amps_3_3v,3.3v_watts=$watts_3_3v $(date '+%s%3N')" > $OUTPUT
+# InfluxDB output to csv format
+echo "power_supply,model=$name,vendor=$vendor temp_0=$temp_0,temp_1=$temp_1,total_time=$powered,uptime=$uptime,supply_volts=$supply,total_watts=$total_watts 12v_volts=$volts_12v,12v_amps=$amps_12v,12v_watts=$watts_12v,5v_volts=$volts_5v,5v_amps=$amps_5v,5v_watts=$watts_5v,3.3v_volts=$volts_3_3v,3.3v_amps=$amps_3_3v,3.3v_watts=$watts_3_3v $(date '+%s%N')" > $OUTPUT
 
 # CSV output
 #echo "measurement,temp_0,temp_1,total_time,uptime,supply_voltage,total_watts,12v_volts,12v_amps,12v_watts,5v_volts,5v_amps,5v_watts,3.3v_volts,3.3v_amps,3.3v_watts" > $OUTPUT
